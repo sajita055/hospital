ba8925a81327ecb18ced439e382422d0832c0168
Sajita Thapa <merritdestiny@gmail.com>
2023-07-10 14:13:16 +0545

    10 july

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,93 @@
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+
+    </ul>
+</nav>
+<!-- /.navbar -->
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <div class="p-2 pt-3 text-center brand-wrapper">
+        <a href="#" class="brand-img-container"><img src="{{asset('assets/dist/img/img.png')}}" alt="AdminLTE Logo" class="img-fluid">
+    </a>
+</div>
+
+    <!-- Sidebar -->
+    <div class="sidebar esSidebar">
+
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+             
+                <li class="nav-item has-submenu">
+                    <a class="nav-link btn btn-outline-secondary" href="#"> Doctors</a>
+                    <ul class="submenu collapse">
+                        <li><a class="nav-link" href="#">All Doctors </a></li>
+                        <li><a class="nav-link" href="#">Add Doctor </a></li>
+                        <li><a class="nav-link" href="#">Doctor profile </a></li>
+                    </ul>
+                </li>
+
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        document.querySelectorAll('.sidebar .nav-link').forEach(function(element) {
+
+            element.addEventListener('click', function(e) {
+
+                let nextEl = element.nextElementSibling;
+                let parentEl = element.parentElement;
+
+                if (nextEl) {
+                    e.preventDefault();
+                    let mycollapse = new bootstrap.Collapse(nextEl);
+
+                    if (nextEl.classList.contains('show')) {
+                        mycollapse.hide();
+                    } else {
+                        mycollapse.show();
+                        // find other submenus with class=show
+                        var opened_submenu = parentEl.parentElement.querySelector(
+                            '.submenu.show');
+                        // if it exists, then close all of them
+                        if (opened_submenu) {
+                            new bootstrap.Collapse(opened_submenu);
+                        }
+                    }
+                }
+            }); // addEventListener
+        }) // forEach
+    });
+</script>
